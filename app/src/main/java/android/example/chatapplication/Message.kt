91dbc2fb8a6bdda03defb3c192bdbf3f6cc6c7a0
Sampada Deore <deoresampada@gmail.com>
2022-07-26 20:19:58 +0530

    ChitChat Chatting app

---
+++ app/src/main/java/android/example/chatapplication/Message.kt
@@ -0,0 +1,13 @@
+package android.example.chatapplication
+
+class Message {
+    var message: String? = null
+    var senderId: String? = null
+
+    constructor(){}
+
+    constructor(message: String?, senderId: String?){
+        this.message = message
+        this.senderId = senderId
+    }
+}
